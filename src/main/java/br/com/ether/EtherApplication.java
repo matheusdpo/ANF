1d862df15a9b403a50ca17051c8b13b7ab0e25e4
Matheus de Paulo Oliveira <matheusoliveira1991@hotmail.com>
2023-10-04 03:45:22 -0300

    anf

---
+++ src/main/java/br/com/ether/EtherApplication.java
@@ -0,0 +1,13 @@
+package br.com.ether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EtherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EtherApplication.class, args);
+	}
+
+}
