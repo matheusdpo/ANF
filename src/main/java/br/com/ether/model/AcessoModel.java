69e7d9195577ff3c608a31cc01cc8ce12a588f80
Matheus de Paulo Oliveira <matheusoliveira1991@hotmail.com>
2023-10-04 03:44:54 -0300

    models done

---
+++ src/main/java/br/com/ether/model/AcessoModel.java
@@ -0,0 +1,13 @@
+package br.com.ether.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class AcessoModel {
+    private String login;
+    private String senha;
+    private String plataforma;
+}
