c91acc349bec2dbf75f5597dd397dfa2c6570440
Matheus de Paulo Oliveira <matheusoliveira1991@hotmail.com>
2023-10-06 00:33:42 -0300

    email class

---
+++ src/main/java/br/com/ether/utilities/EmailUtility.java
@@ -0,0 +1,10 @@
+package br.com.ether.utilities;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class EmailUtility {
+}
