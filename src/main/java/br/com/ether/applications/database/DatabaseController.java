cd19745485872c22bd38dc82c1696bdc542fa0f1
Matheus de Paulo Oliveira <matheusoliveira1991@hotmail.com>
2023-10-10 21:50:25 -0300

    controller para iniciar databse no inicio

---
+++ src/main/java/br/com/ether/applications/database/DatabaseController.java
@@ -0,0 +1,23 @@
+package br.com.ether.applications.database;
+
+
+import br.com.ether.repository.DataBase;
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.Priority;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+@EnableScheduling
+public class DatabaseController {
+    private final DataBase dataBase;
+
+    @PostConstruct
+    @Scheduled(cron = "0 0 * * * *")
+    public void init() {
+        dataBase.connectDB();
+    }
+}
