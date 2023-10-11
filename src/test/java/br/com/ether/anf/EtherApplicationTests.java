f7b89ea632208a05616a857c130b9a2715bf48e9
Matheus de Paulo Oliveira <matheusoliveira1991@hotmail.com>
2023-10-10 23:17:10 -0300

    teste

--- src/test/java/br/com/ether/anf/EtherApplicationTests.java
+++ src/test/java/br/com/ether/anf/EtherApplicationTests.java
@@ -1,19 +1,13 @@
 package br.com.ether.anf;
 
-import br.com.ether.utilities.SeleniumUtility;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@RequiredArgsConstructor
 class EtherApplicationTests {
-private final SeleniumUtility seleniumUtility;
-	@Test
-	void contextLoads() {
-		WebDriver driver = seleniumUtility.getDriver(true, false,"");
-	driver.get("http://www.youtube.com");
-	}
+    @Test
+    void contextLoads() {
+        System.out.println("test");
+    }
 
 }
