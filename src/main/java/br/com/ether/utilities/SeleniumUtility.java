3781c6931f09890f48b25b8b8a51eb9827b7527a
Matheus de Paulo Oliveira <matheusoliveira1991@hotmail.com>
2023-10-17 23:59:25 -0300

    metodos para reduzir linha de codigo

--- src/main/java/br/com/ether/utilities/SeleniumUtility.java
+++ src/main/java/br/com/ether/utilities/SeleniumUtility.java
@@ -2,9 +2,12 @@ package br.com.ether.utilities;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import lombok.RequiredArgsConstructor;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
@@ -38,5 +41,83 @@ public class SeleniumUtility {
         options.addArguments("--accept-notifications");
         options.addArguments("--remote-allow-origins=*");
         return new ChromeDriver(options);
+    }
+
+    public void quit(WebDriver driver) {
+        try {
+            driver.quit();
+        } catch (Exception e) {
+
+        }
+
+        try {
+            driver.close();
+        } catch (Exception ee) {
+
+        }
+    }
+
+    public void sendKeysByXPath(WebDriver driver, WebDriverWait wait, String xpath, String value) {
+        while (true) {
+            try {
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+                driver.findElement(By.xpath(xpath)).sendKeys(value);
+
+                break;
+            } catch (Exception e) {
+                System.out.println("erro ao enviar " + value + " no xpath " + xpath);
+
+            }
+        }
+    }
+
+    public void sendKeysByID(WebDriver driver, WebDriverWait wait, String id, String value) {
+        while (true) {
+            try {
+                driver.findElement(By.id(id)).sendKeys(value);
+
+                break;
+            } catch (Exception e) {
+                System.out.println("erro ao enviar " + value + " no id " + id);
+            }
+        }
+    }
+
+    public void clickElementByXPath(WebDriver driver, WebDriverWait wait, String xpath) {
+        while (true) {
+            try {
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+                driver.findElement(By.xpath(xpath)).click();
+
+                break;
+            } catch (Exception e) {
+                System.out.println("erro ao clicar no xpath " + xpath);
+
+            }
+        }
+    }
+
+    public void clickElementByID(WebDriver driver, WebDriverWait wait, String id) {
+        while (true) {
+            try {
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.id(id)));
+                driver.findElement(By.id(id)).click();
+
+                break;
+            } catch (Exception e) {
+                System.out.println("erro ao clicar no id " + id);
+            }
+        }
+    }
+
+    public void waitByXpath(WebDriver driver, WebDriverWait wait, String xpath) {
+        while (true) {
+            try {
+                wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+                break;
+            } catch (Exception e) {
+                System.out.println("erro ao esperar pelo xpath " + xpath);
+            }
+        }
     }
 }
