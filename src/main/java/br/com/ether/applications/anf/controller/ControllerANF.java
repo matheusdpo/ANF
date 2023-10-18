20cd62d9abc007105c4b99e4b72a92288b6d515b
Matheus de Paulo Oliveira <matheusoliveira1991@hotmail.com>
2023-10-17 23:54:45 -0300

    removido captura de credenciais via banco

--- src/main/java/br/com/ether/applications/anf/controller/ControllerANF.java
+++ src/main/java/br/com/ether/applications/anf/controller/ControllerANF.java
@@ -1,16 +1,18 @@
 package br.com.ether.applications.anf.controller;
 
 import br.com.ether.applications.anf.services.ANFService;
-import br.com.ether.model.CredenciaisModel;
-import br.com.ether.model.DadosDBModel;
+import br.com.ether.model.DatasDBModel;
 import br.com.ether.repository.DataBase;
 import br.com.ether.utilities.DateUtility;
+import br.com.ether.utilities.FolderUtility;
 import br.com.ether.utilities.LogUtility;
 import lombok.RequiredArgsConstructor;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.List;
 
 @Controller
@@ -24,21 +26,22 @@ public class ControllerANF {
 
     @Scheduled(cron = "0 30 13 15 * *")
     public void run() {
-        logger.registraLog("=============================================");
-        logger.registraLog("Iniciando ANF | Mês: " + dateUtility.getToday("MM/yyyy"));
+        logger.registerLog("=============================================");
+        logger.registerLog("Starting ANF | Month: " + dateUtility.getToday("MM/yyyy"));
 
-        List<DadosDBModel> dadosDBModelList = dataBase.getCasos();
+        Instant start = Instant.now();
 
-        if (!dadosDBModelList.isEmpty()) {
-            List<CredenciaisModel> acessosModelList = dataBase.getAcessos();
-            acessosModelList.forEach(e -> {
-                if (e.getPlataforma().equalsIgnoreCase("NF"))
-                    dadosDBModelList.forEach(d -> anfService.run(d, e));
-            });
-        } else {
-            logger.registraLog("Não há casos para serem executados");
-        }
+        List<DatasDBModel> datasDBModelList = dataBase.getCases();
 
-        logger.registraLog("Finalizando ANF | Mês: " + dateUtility.getToday("MM/yyyy"));
+        if (!datasDBModelList.isEmpty()) datasDBModelList.forEach(d -> anfService.run(d));
+        else logger.registerLog("There are no cases to be processed.");
+
+        Instant finish = Instant.now();
+
+        Duration timeElapsed = Duration.between(start, finish);
+        String tempo = String.format("%02d:%02d:%02d.%03d", timeElapsed.toHours(), timeElapsed.toMinutesPart(), timeElapsed.toSecondsPart(), timeElapsed.toMillisPart());
+
+        logger.registerLog("Finish of ANF | Month: " + dateUtility.getToday("MM/yyyy"));
+        logger.registerLog("Finished in " + tempo);
     }
 }
